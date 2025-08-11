1149dd5e235b80f4da5869b01a89ebd18f75da42
Yaswanth babu Patnam <yaswanthpatnam1819@gmail.com>
2025-08-11 22:38:28 +0530

    day 8

---
+++ Day_9/minimum_heights_tower.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumProfit(self, prices):
+        # code here
+        minSoFar = prices[0]
+        res = 0
+        
+        for i in range(1,len(prices)):
+            minSoFar = min(minSoFar,prices[i])
+            res = max(res,prices[i]-minSoFar)
+        return res    
